33f15e65231590d9c6b57df9ce9bf105bd4eb22a
Rafael Garcia-Suarez <rgs@consttype.org>
2014-09-28 08:12:49 +0200

    Add some POD

--- lib/Attribute/RecordCallers.pm
+++ lib/Attribute/RecordCallers.pm
@@ -24,3 +24,25 @@ sub clear {
 }
 
 1;
+
+=head1 NAME
+
+Attribute::RecordCallers - keep a record of who called a subroutine
+
+=head1 SYNOPSIS
+
+    use Attribute::RecordCallers;
+    sub call_me_and_i_ll_tell_you : RecordCallers { ... }
+
+=head1 DESCRIPTION
+
+=head1 LICENSE
+
+(c) Rafael Garcia-Suarez (rgs at consttype dot org) 2014
+
+This program is free software; you may redistribute it and/or modify it under
+the same terms as Perl itself.
+
+A git repository for the sources is at L<https://github.com/rgs/Attribute-RecordCallers>.
+
+=cut
